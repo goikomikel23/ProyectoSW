e4e4b6f5da3ed6c95d1262826e20e9cb73ce4c15
goikomikel23 <EHU@MacBook-Pro-de-Mikel-2.local>
2015-11-13 14:00:36 +0100

    Lab 6 SW Obligatorios OK

--- ComprobarContraseña.php
+++ ComprobarContraseña.php
@@ -1,7 +1,18 @@
 <?php
+if(isset($_GET["q"])){
 
-require_once('lib/nusoap.php');
+$pass=$_GET["q"];
+
+require_once('lib/nusoap.php'); 
 require_once('lib/class.wsdlcache.php');
-
 	
+$soapclient = new nusoap_client( 'http://localhost/ProyectoSW/ComprobarContraseñaWSDL.php?wsdl', false);
+
+$result = $soapclient->call('ComprobarPass',array('Pass'=>$pass));
+
+	echo $result;
+}
+else
+echo "ALGO A FALLADO EN ComprobarContraseña.php";
 ?>
+	
---
+++ ComprobarContraseñaWSDL.php
@@ -0,0 +1,35 @@
+<?php
+	
+//CREAMOS EL WSDL
+
+require_once('lib/nusoap.php');
+require_once('lib/class.wsdlcache.php');
+
+$ns="http://localhost/ProyectoSW/lib/samples/";
+$server = new soap_server;
+$server->configureWSDL('ComprobarPass',$ns);
+$server->wsdl->schemaTargetNamespace=$ns;
+
+
+$server->register('ComprobarPass', array('Pass'=>'xsd:string'),array('response'=>'xsd:string'), $ns);
+
+function ComprobarPass ($Pass){
+	
+	$file = file('http://localhost/ProyectoSW/otros/toppasswords.txt',FILE_IGNORE_NEW_LINES);
+	
+	$boo = "Valida";
+	
+	foreach($file as $num_linea=>$linea){
+		if ($linea==$Pass)
+			$boo = "Invalido";		
+	}
+	
+	return $boo;
+	
+}
+
+$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA:''; 
+$server->service($HTTP_RAW_POST_DATA);
+
+	
+?>
